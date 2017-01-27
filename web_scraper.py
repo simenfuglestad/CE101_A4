87ccd77c2169556f0186d6a0d33650a8994ac898
sifusan <fuglestad.simen@gmail.com>
2017-01-27 15:57:15 +0000

    changed compatability to python 2.x, replaced tabs with spaces

--- web_scraper.py
+++ web_scraper.py
@@ -1,31 +1,40 @@
-import urllib.request
+import urllib2
 from bs4 import BeautifulSoup
 
+
 def format_web_string(string):
-	cache =""
-	found_tag = False
-	for c in string:
-		if c == '<':
-			found_tag = True
+    cache = ""
+    found_tag = False
+    for c in string:
+        if c == '<':
+            found_tag = True
 
-		if c == '>':
-			found_tag = False
-			cache = cache + c
+        if c == '>':
+            found_tag = False
+            cache = cache + c
 
-		if found_tag:
-			cache = cache +c
+        if found_tag:
+            cache = cache + c
 
-		if not found_tag and len(cache) > 0:
-			string = string.replace(cache, "")
-			cache = ""
-	return string
+        if not found_tag and len(cache) > 0:
+            string = string.replace(cache, "")
+            cache = ""
+    return string
+
+
+def eliminate_single_word_lines(string):
+    for c in string:
+        pass
+
 
 def get_string_from_url(url):
-	website = urllib.request.urlopen(url).read()
-	soup = BeautifulSoup(website, "lxml")
-	string = ""
-	for p in soup.findAll('p'):
-		string = string + str(p) + '\n'
-	return string
+    website = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(website, "lxml")
+    string = ""
+    for p in soup.findAll('p'):
+        string = string + str(p) + '\n'
+    string = str(soup.findAll('title')) + string
+    return string
 
-print(format_web_string(get_string_from_url("http://www.bbc.co.uk/news/uk-politics-38286794")))
+print format_web_string(get_string_from_url(
+        "http://www.bbc.co.uk/news/uk-england-leeds-38767596"))
